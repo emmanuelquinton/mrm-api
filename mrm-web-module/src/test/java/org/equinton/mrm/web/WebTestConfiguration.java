2e0b39ab11cd9c112ad8bab727f0a68214834b68
Emmanuel <equinton.pro@gmail.com>
2020-04-16 07:35:53 +0200

    feat(user): add user in web module

--- mrm-web-module/src/test/java/org/equinton/mrm/web/WebTestConfiguration.java
+++ mrm-web-module/src/test/java/org/equinton/mrm/web/WebTestConfiguration.java
@@ -1,9 +1,13 @@
 package org.equinton.mrm.web;
 
 import org.equinton.mrm.domain.adapter.ReservationAdapter;
+import org.equinton.mrm.domain.adapter.UserAdapter;
 import org.equinton.mrm.domain.model.Reservation;
+import org.equinton.mrm.domain.model.User;
 import org.equinton.mrm.domain.port.ReservationDomainService;
 import org.equinton.mrm.domain.port.ReservationRecord;
+import org.equinton.mrm.domain.port.UserDomainService;
+import org.equinton.mrm.domain.port.UserRecord;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -22,13 +26,28 @@ public class WebTestConfiguration extends WebConfiguration {
     ReservationDomainService reservationDomainService(ReservationRecord reservationRecord) {
         return new ReservationAdapter(reservationRecord);
     }
+    @Bean
+    public UserRecord userRecord() {
+        return new UserRecordAdapter();
+    }
 
-    private class ReservationAdapterImpl implements ReservationRecord {
+    @Bean
+    public UserDomainService userDomainService(UserRecord userRecord) {
+        return new UserAdapter(userRecord);
+    }
+    public class ReservationAdapterImpl implements ReservationRecord {
 
         @Override
         public Reservation save(Reservation reservation) {
 
             return reservation;
+        }
+    }
+
+    public class UserRecordAdapter implements UserRecord {
+        @Override
+        public User save(User user) {
+            return user;
         }
     }
 }
--- mrm-web-module/src/test/java/org/equinton/mrm/web/controller/UserControllerTest.java
+++ mrm-web-module/src/test/java/org/equinton/mrm/web/controller/UserControllerTest.java
@@ -1,16 +1,27 @@
 package org.equinton.mrm.web.controller;
 
-import org.equinton.mrm.domain.port.ReservationDomainService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.http.ContentType;
+import org.equinton.mrm.domain.model.User;
+import org.equinton.mrm.domain.port.UserDomainService;
+import org.equinton.mrm.web.dto.UserDto;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.http.HttpStatus;
 
 import javax.annotation.PostConstruct;
+import java.util.UUID;
 
-@ExtendWith(SpringExtension.class)
+import static io.restassured.RestAssured.given;
+
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTest {
     @LocalServerPort
@@ -23,5 +34,62 @@ public class UserControllerTest {
         uri = "http://localhost:" + port;
     }
 
+    @InjectMocks
+    private UserController usernController;
 
+    @Mock
+    private UserDomainService userDomainService;
+
+
+
+    @Test
+    void should_post_user() throws JsonProcessingException {
+        UserDto userToSave = UserDto.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .serialNumber("ld-015987")
+                .build();
+        ObjectMapper mapper = new ObjectMapper();
+        String payload = mapper.writeValueAsString(userToSave);
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(payload)
+                .when()
+                .post(uri + "/users")
+                .then()
+                .log().ifError()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void shoudl_call_save_method_of_domaine_service() throws JsonProcessingException {
+        //given
+        UserDto userDtoToSave = UserDto.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .serialNumber("ld-015987")
+                .build();
+
+
+        User userToSave = userDtoToSave.toUser();
+        User savedUser = User.builder()
+                .id(UUID.randomUUID())
+                .firstName("John")
+                .lastName("Doe")
+                .serialNumber("ld-015987")
+                .build();
+        ObjectMapper mapper = new ObjectMapper();
+        String payload = mapper.writeValueAsString(userDtoToSave);
+        Mockito.when(userDomainService.save(userToSave))
+                .thenReturn(savedUser);
+
+        //when
+        usernController.createUser(userDtoToSave);
+
+        //then
+        Mockito.verify(userDomainService).save(userToSave);
+
+
+    }
 }
